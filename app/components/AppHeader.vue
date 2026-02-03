8a8e4c385fc397f4410468066c873a9e2461f91c
Magnus Bengtsson <magnusb99@hotmail.se>
2026-02-03 17:18:06 +0100

    Simplify CartView component usage in AppHeader

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -7,7 +7,7 @@
     <UNavigationMenu :items="items" />
 
     <template #right>
-      <CartView class="absolute bottom-10 right-10  " />
+      <CartView />
     </template>
   </UHeader>
 </template>
